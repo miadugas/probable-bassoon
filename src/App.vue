ffdebea319035354740ea4e7f519f5f4d23403ed
Mia Dugas <miadugas@Mias-MacBook-Air.local>
2023-05-16 21:12:50 -0600

    poc1

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<Navbar />
+	<div>
+		<WebViewer
+			initialDoc="https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf" />
+	</div>
+</template>
+
+<script lang="ts">
+	import Navbar from './components/Navbar.vue';
+	import WebViewer from './components/WebViewer.vue';
+
+	export default {
+		name: 'App',
+		components: {
+			WebViewer,
+			Navbar,
+		},
+	};
+</script>
+
+<style scoped></style>
---
+++ src/components/WebViewer.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="viewer-container">
+    <div id="webviewer" ref="viewer"></div>
+  </div>
+</template>
+
+
+<script lang="ts">
+	import { ref, onMounted } from 'vue'
+	import WebViewer from '@pdftron/webviewer'
+
+	export default {
+	  name: 'WebViewer',
+	  props: { initialDoc: { type: String }},
+	  setup(props) {
+	    const viewer = ref(null)
+	    onMounted(() => {
+	      const path = `${import.meta.env.BASE_URL}webviewer`;
+	      WebViewer({ path, initialDoc: props.initialDoc }, viewer.value);
+	    })
+	    return { viewer }
+	  },
+	}
+</script>
+
+
+<style>
+.viewer-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  padding: 5px; /* This will act as a margin around the webviewer */
+  box-sizing: border-box;
+}
+
+#webviewer {
+  height: 100%;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 5px;
+
+}
+
+@media (max-width: 768px) {
+  #webviewer {
+    height: calc(80vh - 20px);
+    width: calc(90vw - 20px);
+    padding: 5px;
+  }
+}
+
+</style>
